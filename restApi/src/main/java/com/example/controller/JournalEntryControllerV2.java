3663ec83689cc7fa2f83d0c1e6ce0bd91c288923
Ali Shan <alishanarshad14@gmail.com>
2025-02-18 12:00:17 +0500

    pom file fixed

---
+++ restApi/src/main/java/com/example/controller/JournalEntryControllerV2.java
@@ -0,0 +1,50 @@
+package com.example.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.JournalEntry;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryControllerV2 {
+
+    @GetMapping
+    public List <JournalEntry> getAll(){
+        return null;
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        return false;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId) {
+        return null;
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleJournalEntryById(@PathVariable Long myId){
+        return null;
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry) {
+        return null;
+    }
+    
+
+}
